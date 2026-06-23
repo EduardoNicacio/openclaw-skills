9b352d7f99005f9d07f49f285a55cf43715bb130
Vincent Koc <25068+vincentkoc@users.noreply.github.com>
2026-06-23 17:56:48 +0800

    fix(routing): protect publisher aliases from plugin collisions

--- packages/schema/dist/index.js
+++ packages/schema/dist/index.js
@@ -4,6 +4,7 @@ export * from "./catalogMetadata.js";
 export * from "./docsLinks.js";
 export * from "./license.js";
 export * from "./openclawContract.js";
+export * from "./openClawExtensionSlugs.js";
 export * from "./packages.js";
 export * from "./pluginCategories.js";
 export { ApiRoutes, LegacyApiRoutes } from "./routes.js";
